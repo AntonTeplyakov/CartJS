9dc4b21a3119a5c2729f4d38c510f12508558fc1
AntonTeplyakov <jsxscript@gmail.com>
2021-11-17 19:21:09 +1000

    made simple cart

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded',function(){
     let products = document.querySelectorAll('.product'),
     buttons = document.querySelectorAll('button'),
-    open = document.querySelector('.open');
+    openBtn = document.querySelector('.open');
 
     function createCart(){
         let cart = document.createElement('div'),
@@ -23,4 +23,30 @@ window.addEventListener('DOMContentLoaded',function(){
     };
 
     createCart();
+
+    let field = document.querySelector('.cart-field');
+        cart = document.querySelector('.cart');
+        close = document.querySelector('.close');
+
+    function openCart(){
+        cart.style.display = 'block';
+    }
+
+    function closeCart(){
+        cart.style.display = 'none';
+    }
+
+    openBtn.addEventListener('click', openCart);
+    close.addEventListener('click', closeCart);
+
+    buttons.forEach(function(item,i){
+        item.addEventListener('click', function(){
+            let item = products[i].cloneNode(true),
+                btn = item.querySelector('button');
+
+            btn.remove();
+            field.appendChild(item);
+            products[i].remove();
+        });
+    }); 
 });
